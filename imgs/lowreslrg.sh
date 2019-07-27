aa8aa74cb0631239b00931fdfaa628e53218710a
Vega Deftwing <johnathan698@gmail.com>
2019-07-27 11:43:18 -0500

    Removed files for lowres red/cyan idea.

--- imgs/lowreslrg.sh
+++
@@ -1,1 +0,0 @@
-mogrify -depth 4 -quality 70 -dither FloydSteinberg -define dither:diffusion-amount=50% -remap redblue.png -auto-level -quantize RGB -black-threshold 5% -black-point-compensation -posterize 5 -normalize -format png ./imgsoriglrg/*.*
